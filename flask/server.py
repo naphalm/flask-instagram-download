066bd3e58d1ce8e4cf9ef0fca8e20d4bcd0bc2c7
naphalm <master.sys.vp@gmail.com>
2026-02-14 00:24:44 +0200

    Update server.py

--- flask/server.py
+++ flask/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory, abort
 import sys
+import os
 # gunicorn --workers 3 --bind 0.0.0.0:8000 server:app
 
 # sudo systemctl restart gunicorn
