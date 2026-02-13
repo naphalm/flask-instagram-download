a49f840f42170f687867ee831dfa8303511632e9
naphalm <master.sys.vp@gmail.com>
2026-02-14 00:27:18 +0200

    Update server.py

--- flask/server.py
+++ flask/server.py
@@ -30,7 +30,7 @@ def download_reel():
     if not input_url:
         return jsonify({"error": "Missing 'url' parameter"}), 400
 
-    reel_id = extract_reel_id(input_url)
+    reel_id = instagram.extract_reel_id(input_url)
     if not reel_id:
         return jsonify({"error": "Invalid Instagram URL"}), 400
     
